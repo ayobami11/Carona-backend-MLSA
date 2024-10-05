3eb46efd83261996a6642f20219188937a386dad
Akorede <akoredeadewole8@gmail.com>
2024-10-05 19:20:34 +0100

    completed errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,26 +1,34 @@
 import { StatusCodes } from "http-status-codes"
 import { errorResponse } from "../utils/responses.js"
+import mongoose from "mongoose"
+import logger from '../utils/logger.js'
 
 const errorHandler = (error, req, res, next) => {
-    let message = "Something doesn't work"
+    let message = "Request failed. Try again later"
     let errCode = StatusCodes.INTERNAL_SERVER_ERROR
 
 
-
-    if (error instanceof Error){
-        if (error.name === 'ValidationError'){
-            message = error.message
-            errCode = StatusCodes.UNPROCESSABLE_ENTITY
-        }else if (error.name === 'MongoServerError'){
-            if (error.errorResponse.code === 11000){
-                message = "Resource already exists"
-                errCode = StatusCodes.CONFLICT
-            }
+    // mongoose errors
+    if (error instanceof mongoose.Error){
+        message = error.message
+        errCode = StatusCodes.BAD_REQUEST
+    }else if (
+        error instanceof RangeError ||
+        error instanceof TypeError ||
+        error instanceof URIError ||
+        error instanceof ReferenceError ||
+        error instanceof SyntaxError ||
+        error instanceof AggregateError ||
+        error instanceof EvalError
+    ){
+        message = error.message
+        errCode = StatusCodes.BAD_REQUEST
+    }else if (error.name === 'MongoServerError' && error.errorResponse.code === 11000){
+        message = "Resource already exists"
+        errCode = StatusCodes.CONFLICT
     }
-}
 
-
-
+    logger.error(`[${req.method} ${req.url}] ${typeof message === 'string' ? message: JSON.stringify(message)}`)
 
     errorResponse(res, errCode, message)
 
