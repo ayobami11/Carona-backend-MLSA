78d1f32fe6773ad28bb851bdbd69365cdd47c186
Abdulbaasiterinkitola <eabdulbaasit@gmail.com>
2024-10-14 07:02:47 +0100

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import { changePassword, loginAccount, registerAccount } from "../controllers/auth.js";
-
-const authRouter = Router()
-authRouter.post('/register', registerAccount)
-authRouter.post("/login", loginAccount)
-authRouter.patch("/change-password", changePassword)
 import { signUpValidation } from "../middlewares/authValidations.js";
+
 
 const authRouter = Router()
 authRouter.post('/register', signUpValidation, registerAccount)
+authRouter.post("/login", loginAccount)
+authRouter.patch("/change-password", changePassword)
 
 
 export default authRouter
